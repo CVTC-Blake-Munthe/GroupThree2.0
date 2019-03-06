2eec81663ff2e31e7dbb282b2eda1d0af241ae2e
Blake Munthe <bmunthe1@student.cvtc.edu>
2019-03-06 16:14:00 -0600

    Added files from original project

---
+++ GroupThreeApp/GroupThreeApp/WpfApp1/WpfApp1/GuestDBContext.cs
@@ -0,0 +1,21 @@
+﻿using AgileTesting.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileTesting
+{
+    class GuestDBContext : DbContext
+    {
+        public DbSet<Guest> guests { get; set; }
+        public DbSet<Calendar> calendars { get; set; }
+        public DbSet<Stay> stays { get; set; }
+        public DbSet<Rule> rules { get; set; }
+        public DbSet<Strike> strikes { get; set; }
+        public DbSet<EmergencyContact> emergencyContact { get; set; }
+
+    }
+}
