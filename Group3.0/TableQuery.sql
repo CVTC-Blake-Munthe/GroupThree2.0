b16982b8b1875de926116a41701b6bbde49b0bb6
Blake Munthe <bmunthe1@student.cvtc.edu>
2019-03-13 13:06:19 -0500

    Removed Unneeded Project

---
+++ Group3.0/TableQuery.sql
@@ -0,0 +1,77 @@
+﻿DROP TABLE IF EXISTS Guest
+CREATE TABLE [dbo].[Guest] (
+[GuestID] [int] NOT NULL IDENTITY,
+[FirstName] [nvarchar](max),
+[MiddleInitial] [nvarchar](max),
+[LastName] [nvarchar](max),
+[Gender] [nvarchar](max),
+[DateOfBirth] [datetime],
+[SocialSecurity] [nvarchar](max),
+[PhoneNumber] [nvarchar](max),
+[Services] [nvarchar](max),
+[Picture] [nvarchar](max),
+[IntakeComplete] [BIT],
+CONSTRAINT [PK_dbo.Guest] PRIMARY KEY ([GuestID])
+)
+
+DROP TABLE IF EXISTS Calendar
+CREATE TABLE [dbo].Calendar (
+[CalendarID] [int] NOT NULL IDENTITY,
+[CalendarDate] [datetime],
+CONSTRAINT [PK_dbo.Calendar] PRIMARY KEY ([CalendarID])
+)
+
+
+DECLARE @StartDate DATETIME
+DECLARE @EndDate DATETIME
+SET @StartDate = GETDATE()
+SET @EndDate = DATEADD(d, 365, @StartDate)
+
+WHILE @StartDate <= @EndDate
+      BEGIN
+             INSERT INTO [Calendar]
+             (
+                   CalendarDate
+             )
+             SELECT
+                   @StartDate
+
+             SET @StartDate = DATEADD(dd, 1, @StartDate)
+      END
+
+DROP TABLE IF EXISTS EmergencyContact
+CREATE TABLE [dbo].EmergencyContact (
+[ContactID] [int] NOT NULL IDENTITY,
+[GuestID] [int] NOT NULL,
+[ContactFName] [nvarchar](max),
+[ContactLName] [nvarchar](max),
+[ContactPhone] [nvarchar](max),
+[ContactRelation] [nvarchar](max),
+CONSTRAINT [PK_dbo.EmergencyContact] PRIMARY KEY ([ContactID])
+)
+
+DROP TABLE IF EXISTS Rules
+CREATE TABLE [dbo].Rules (
+[RuleID] [int] NOT NULL IDENTITY,
+[RuleNo] [int],
+[RuleText] [nvarchar](max),
+CONSTRAINT [PK_dbo.Rules] PRIMARY KEY ([RuleID])
+)
+
+DROP TABLE IF EXISTS Stays
+CREATE TABLE [dbo].Stays (
+[GuestID] [int] NOT NULL,
+[CalendarID] [int] NOT NULL,
+CONSTRAINT [PK_dbo.Stays] PRIMARY KEY ([GuestID],[CalendarID])
+)
+
+DROP TABLE IF EXISTS Strikes
+CREATE TABLE [dbo].Strikes (
+[StrikeID] [int] NOT NULL IDENTITY,
+[GuestID] [int] NOT NULL,
+[CalendarID] [int] NOT NULL,
+[RuleID] [int] NOT NULL,
+[Notes] [nvarchar](max),
+[StrikeOutWarning] [nvarchar](max),
+CONSTRAINT [PK_dbo.Strikes] PRIMARY KEY ([StrikeID])
+)
---
+++ Group3.0/WpfApp1/Models/EmergencyContact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileTesting.Models
+{
+    [Table("emergencyContacts")]
+    public class EmergencyContact
+    {
+        [Key]
+        public int ContactID { get; set; }
+        
+        [ForeignKey("guests")]
+        public int GuestID { get; set; }
+        public Guest guests { get; set; }
+        
+        public string ContactFname { get; set; }
+        public string ContactLname { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactRelation { get; set; }
+
+    }
+}
